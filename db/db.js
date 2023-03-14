0c93bd16947fa5795054c37e66693b1bb7305800
mcpeck <peckmelodie@gmail.com>
2023-03-14 16:34:16 -0500

    Created function for fetching questions from db in db.js

--- db/db.js
+++ db/db.js
@@ -1,33 +1,48 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/qa');
+require('dotenv').config()
+const { Client } = require('pg');
 
-const questionSchema = new Schema({
-  product_id: Number,
-  results: [{
-    question_id: Number,
-    question_body: String,
-    question_date: Date,
-    question_helpfulness: Number,
-    reported: Boolean
-  }]
+const client = new Client({
+  database: 'postgres',
+  user: process.env.POSTGRESQL_USER,
+  password: process.env.POSTGRESQL_PASSWORD
 });
 
-const answerSchema = new Schema({
-  question_id: Number,
-  answer_id: Number,
-  body: String,
-  date: Date,
-  answerer_name: String,
-  helpfulness: Number
-});
+client.connect();
 
-const photoSchema = new Schema({
-  answer_id: Number,
-  id: Number,
-  url: String
-});
+function resolveQuestions(pId) {
+  const qQuery = 'SELECT * FROM questions WHERE reported = 0 AND product_id =';
+  return client.query(qQuery + pId);
+}
 
-const Questions = mongoose.model('Questions', questionSchema);
-const Answers = mongoose.model('Answers', answerSchema);
-const Photos = mongoose.model('Questions', photoSchema);
+function resolveAnswers(qId) {
+  const aQuery = 'SELECT * FROM answers WHERE q_id ='
+  return client.query(aQuery + qId);
+}
+
+function resolvePhotos(aId) {
+  const pQuery = 'SELECT * FROM answers_photos WHERE answer_id ='
+  return client.query(pQuery + aId);
+}
+
+module.exports.getQuestions = async (productId) => {
+  var response = {
+    product_id: productId
+  };
+
+  var qs = await resolveQuestions(productId);
+  var fullQs = qs.rows.map(async (q, index) => {
+    q.answers = {};
+    var as = await resolveAnswers(q.question_id);
+    for (var i = 0; i < as.rows.length; i++) {
+      let a = as.rows[i];
+      let photos = await resolvePhotos(a.id);
+      a.photos = photos.rows;
+      q.answers[a.id] = a;
+    }
+    return q;
+  });
+
+  response.results = await Promise.all(fullQs);
+  return response;
+};
 
---
+++ server/index.js
@@ -0,0 +1,3 @@
+const { getQuestions } = require('../db/db.js');
+
+return getQuestions("5");
