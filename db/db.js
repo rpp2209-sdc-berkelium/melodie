ed87a4972a65a7da83ac809e6bbf0f49d6fde566
mcpeck <peckmelodie@gmail.com>
2023-03-18 18:54:34 -0500

    Complete server routes and db methods

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const { Client } = require('pg');
 
 const client = new Client({
-  database: 'postgres',
+  database: 'testqa',
   user: process.env.POSTGRESQL_USER,
   password: process.env.POSTGRESQL_PASSWORD
 });
@@ -27,6 +27,45 @@ function resolvePhotos(aId) {
   return client.query(pQuery, values);
 }
 
+function insertQuestion(qData) {
+  const insertQuery = 'INSERT INTO questions (question_body, asker_name, asker_email, product_id) VALUES($1, $2, $3, $4)';
+  const values = [qData.body, qData.name, qData.email, qData.product_id];
+  return client.query(insertQuery, values);
+};
+
+async function insertAnswer(qId, aData) {
+  const insertQuery = 'INSERT INTO answers (q_id, body, answerer_name, answerer_email) VALUES($1, $2, $3, $4)';
+  const values = [qId, aData.body, aData.name, aData.email];
+  await client.query(insertQuery, values);
+
+  var pPromises = aData.photos.map((photo) => {
+    const pQuery = 'INSERT INTO answers_photos (answer_id, url) VALUES((SELECT last_value FROM answers_answer_id_seq), $1)';
+    const pValues = [photo];
+    return client.query(pQuery, pValues);
+  });
+  return Promise.all(pPromises);
+}
+
+function incrementQuestionHelpfulness(qId) {
+  const iQuery = 'UPDATE questions SET question_helpfulness = question_helpfulness + 1 WHERE question_id = $1';
+  return client.query(iQuery, [qId]);
+};
+
+function markQuestionReported(qId) {
+  const rQuery = 'UPDATE questions SET reported = true WHERE question_id = $1';
+  return client.query(rQuery, [qId]);
+};
+
+function incrementAnswerHelpfulness(aId) {
+  const iQuery = 'UPDATE answers SET helpfulness = helpfulness + 1 WHERE id = $1';
+  return client.query(iQuery, [aId]);
+};
+
+function markAnswerReported(aId) {
+  const rQuery = 'UPDATE answers SET reported = true WHERE id = $1;';
+  return client.query(rQuery, [aId]);
+};
+
 const getQuestions = async (productId, page, count) => {
   var qs = await resolveQuestions(productId, page, count);
   var fullQs = qs.rows.map(async (q, index) => {
@@ -50,5 +89,35 @@ const getAnswers = async (questionId, callback, page, count) => {
   }
 }
 
+const addQuestion = async (qData) => {
+  await insertQuestion(qData);
+};
+
+const addAnswer = async (qId, aData) => {
+  await insertAnswer(qId, aData);
+};
+
+const markQuestionHelpful = async (qId) => {
+  await incrementHelpfulness(qId);
+};
+
+const reportQuestion = async (qId) => {
+  await markQuestionReported(qId);
+};
+
+const markAnswerHelpful = async (aId) => {
+  await incrementAnswerHelpfulness(aId);
+};
+
+const reportAnswer = async (aId) => {
+  await markAnswerReported(aId);
+}
+
 module.exports.getQuestions = getQuestions;
 module.exports.getAnswers = getAnswers;
+module.exports.addQuestion = addQuestion;
+module.exports.addAnswer = addAnswer;
+module.exports.markQuestionHelpful = markQuestionHelpful;
+module.exports.reportQuestion = reportQuestion;
+module.exports.markAnswerHelpful = markAnswerHelpful;
+module.exports.reportAnswer = reportAnswer;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
-const { getQuestions, getAnswers } = require('../db/db.js');
+const { getQuestions, getAnswers, addQuestion, addAnswer, markQuestionHelpful, reportQuestion, markAnswerHelpful, reportAnswer} = require('../db/db.js');
 
 app.use(express.json());
 
@@ -17,7 +17,7 @@ app.get('/qa/questions', (req, res) => {
     })
   })
   .catch((err) => {
-    res.status(400).send('An error occurred getting questions' + err);
+    res.status(400).send('An error occurred getting questions. ' + err);
   })
 });
 
@@ -37,7 +37,67 @@ app.get('/qa/questions/:question_id/answers', (req, res) => {
     })
   })
   .catch((err) => {
-    res.status(400).send('An error occurred getting answers' + err);
+    res.status(400).send('An error occurred getting answers. ' + err);
+  })
+});
+
+app.post('/qa/questions', (req, res) => {
+  return addQuestion(req.body)
+  .then(() => {
+    res.sendStatus(201);
+  })
+  .catch((err) => {
+    res.status(400).send('An error occurred adding a question. ' + err);
+  })
+});
+
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+  return addAnswer(req.params.question_id, req.body)
+  .then(() => {
+    res.sendStatus(201);
+  })
+  .catch((err) => {
+    res.status(400).send('An error occurred adding an answer. ' + err);
+  })
+});
+
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  return markQuestionHelpful(req.params.question_id)
+  .then(() => {
+    res.sendStatus(204);
+  })
+  .catch((err) => {
+    res.status(400).send('An error occurred marking a question as helpful. ' + err);
+  })
+});
+
+app.put('/qa/questions/:question_id/report', (req, res) => {
+  return reportQuestion(req.params.question_id)
+  .then(() => {
+    res.sendStatus(204);
+  })
+  .catch((err) => {
+    res.status(400).send('An error occurred reporting a question. ' + err);
+  })
+});
+
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  return markAnswerHelpful(req.params.answer_id)
+  .then(() => {
+    res.sendStatus(204);
+  })
+  .catch((err) => {
+    res.status(400).send('An error occurred marking an answer as helpful. ' + err);
+  })
+});
+
+app.put('/qa/answers/:answer_id/report', (req, res) => {
+  return reportAnswer(req.params.answer_id)
+  .then(() => {
+    res.sendStatus(204);
+  })
+  .catch((err) => {
+    res.status(400).send('An error occurred reporting an answer. ' + err);
   })
 });
 
